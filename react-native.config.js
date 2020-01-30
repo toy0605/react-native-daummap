10b0ed02d20d19487328a3fa3e4eb367fbbad196
이승렬 <toy0605@scon.app>
2020-01-30 16:00:18 +0900

    rnpm -> react-native 마이그레이션

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    project: {
+      ios: {
+        "sharedLibraries": [
+            "libxml2",
+            "libsqlite3",
+            "SystemConfiguration",
+            "CoreLocation",
+            "OpenGLES",
+            "QuartzCore",
+            "libc++"
+        ]
+      }
+    }
+};
